59557737768e04b7ab9ec2bd317b89de6e7f7871
Baltazar Molteni <baltazarmolteni@alu.frlp.utn.edu.ar>
2025-04-15 13:16:48 -0300

    [Parte 4.1] Agrego formulario en template con vista correspondiente

--- proyecto/encuestas/urls.py
+++ proyecto/encuestas/urls.py
@@ -6,6 +6,6 @@ app_name = "encuestas"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detalle, name="detalle"),
-    path("<int:question_id>/resultados/", views.resultados, name="resultado"),
+    path("<int:question_id>/resultados/", views.resultados, name="resultados"),
     path("<int:question_id>/voto/", views.voto, name="voto"),
 ]
--- proyecto/encuestas/views.py
+++ proyecto/encuestas/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from .models import Encuesta
-
+from .models import Encuesta, Respuesta
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Encuesta.objects.order_by("-fecha_publ")[:5]
@@ -14,9 +15,28 @@ def detalle(request, question_id):
     return render(request, "encuestas/detalle.html", {"encuesta": question})
 
 def resultados(request, question_id):
-    response = "Estás viendo las respuestas a la pregunta %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Encuesta, pk=question_id)
+    return render(request, "encuestas/resultados.html", {"encuesta": question})
 
 
 def voto(request, question_id):
-    return HttpResponse("Estás votando en la pregunta %s." % question_id)
+    question = get_object_or_404(Encuesta, pk=question_id)
+    try:
+        selected_choice = question.respuesta_set.get(pk=request.POST["respuesta"])
+    except (KeyError, Respuesta.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "encuestas/detalle.html",
+            {
+                "encuesta": question,
+                "error_message": "No seleccionaste una opción.",
+            },
+        )
+    else:
+        selected_choice.votos = F("votos") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("encuestas:resultados", args=(question.id,)))
