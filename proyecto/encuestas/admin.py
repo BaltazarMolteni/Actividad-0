7791ef28665dc8dfb9faf4a67858a257c0c355c8
Baltazar Molteni <baltazarmolteni@alu.frlp.utn.edu.ar>
2025-04-15 12:19:12 -0300

    [Parte 2.1] Creo modelos y los agrego a admin

--- proyecto/encuestas/admin.py
+++ proyecto/encuestas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Encuesta
+
+admin.site.register(Encuesta)
--- proyecto/encuestas/models.py
+++ proyecto/encuestas/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Encuesta(models.Model):
+    texto_encuesta = models.CharField(max_length=200)
+    fecha_publ = models.DateTimeField("fecha de publicacion")
+    def __str__(self):
+        return self.texto_encuesta
+    def fue_publicado_recientemente(self):
+        return self.fecha_publ >= timezone.now() - datetime.timedelta(days=1)
+
+class Respuesta(models.Model):
+    encuesta = models.ForeignKey(Encuesta, on_delete=models.CASCADE)
+    texto_respuesta = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
+    def __str__(self):
+        return self.texto_respuesta
